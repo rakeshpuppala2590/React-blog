bae5006032155b5b1e3be3a6ed8c5ce27a67af9b
Rakesh Puppala <rakeshpuppala2591.com>
2024-05-29 12:58:44 -0700

    router

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 const useFetch = (url) =>{
+
+
     const [data, setData] = useState(null)
     const [isPending, setIsPending] =  useState(true);
     const [error, setError] = useState(null);
     useEffect(()=> {
+
+        const abortCont = new AbortController();
+
         setTimeout(()=>{
-          fetch(url)
+          fetch(url, {signal: abortCont.signal})
           .then(res => {
             if(!res.ok){
               throw Error('Could not fetch the data for the resource');
@@ -19,11 +24,17 @@ const useFetch = (url) =>{
             setError(null);
           })
           .catch(err => {
+            if(err.name === 'AbortError'){
+                console.log('fetch aborted')
+            }
             setIsPending(false);
             setError(err.message);
           })
         }, 1000)
-      }, []); 
+
+        return () => abortCont.abort();
+
+      }, [url]); 
 
       return {data, isPending, error}
 }
